c43e1fc2153cfccd342ff9296ae6932cdb8be1c3
NewbieTop <71062404+qkrfowns45@users.noreply.github.com>
2022-12-29 20:51:44 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+//0.React엔진 -데이터변경감지에서 UI그려주는 엔진
+//1.실행과정(index.html) - spa(싱글 페이지 애플리케이션) a태크 못적음!
+//2.JSX 문법
+//3. 바벨(자바스크립트 ES5) -> ES6 함수형으로 가능!
+function App() {
+  return <div>안녕1sss</div>;
+}
+
+export default App;
