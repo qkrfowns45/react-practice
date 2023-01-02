12d4e10c1312b613dc1a93c7f3d703b77ad8d0cd
NewbieTop <71062404+qkrfowns45@users.noreply.github.com>
2023-01-02 21:21:08 +0900

    components styled practice

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+//디자인을 만들때는 app()안에 만들면 안된다. 정적파일이기때문에
+
+import styled from 'styled-components';
+import './App.css';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Title>안녕</Title>
+      <div>헬로</div>
+    </div>
+  );
 }
 
 export default App;
