767140b370d3435f581c394413c1971e43ba0b94
NewbieTop <71062404+qkrfowns45@users.noreply.github.com>
2023-01-08 21:10:51 +0900

    글쓰기, 글목록보기 practice

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 //글쓰기, 글삭제, 글목록보기
-import { Route } from 'react-router-dom';
-import Navigation from './components/Navigation';
 import ListPage from './pages/ListPage';
-import WritePage from './pages/WritePage';
 
 function App() {
   return (
     <div>
-      <Navigation />
-      <Route path="/" exact={true} component={ListPage} />
-      <Route path="/write" exact={true} component={WritePage} />
+      <ListPage />
     </div>
   );
 }
--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -12,21 +12,63 @@ const StyledBoxDix = styled.div`
 `;
 
 const ListPage = () => {
-  const [posts, setPost] = useState([
-    { id: 1, title: '내용1' },
-    { id: 2, title: '내용2' },
-    { id: 3, title: '내용3' },
-    { id: 4, title: '내용4' },
-    { id: 5, title: '내용5' },
+  const [no, setNo] = useState(6);
+
+  const [post, setPost] = useState({
+    id: no,
+    title: '',
+    content: '',
+  });
+  const [posts, setPosts] = useState([
+    { id: 1, title: '제목1', content: '내용1' },
+    { id: 2, title: '제목2', content: '내용2' },
+    { id: 3, title: '제목3', content: '내용3' },
+    { id: 4, title: '제목4', content: '내용4' },
+    { id: 5, title: '제목5', content: '내용5' },
   ]);
+
+  const handleWrite = () => {
+    setPosts([...posts, { ...post, id: no }]);
+    setNo(no + 1);
+  };
+
+  const handleForm = (e) => {
+    console.log(e.target.name);
+    console.log(e.target.value);
+
+    //computed property names 문법 (키값 동적할당) 기존값 유지하면서 덮어쓰기해야한다.
+    setPost({
+      ...post,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <div>
       <h1>리스트페이지 입니다.</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="제목을 입력하세요..."
+          value={post.title}
+          onChange={handleForm}
+          name="title"
+        />
+        <input
+          type="text"
+          placeholder="내용을 입력하세요..."
+          value={post.content}
+          onChange={handleForm}
+          name="content"
+        />
+        <button type="button" onClick={handleWrite}>
+          글쓰기
+        </button>
+      </form>
       <hr />
       {posts.map((post) => (
         <StyledBoxDix>
-          번호 : {post.id} 제목 : {post.title}
+          번호 : {post.id} / 제목 : {post.title} / 내용 : {post.content}
           <div>
             <button>삭제</button>
           </div>
